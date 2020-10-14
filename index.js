a7da28ffc521f533393caffb73bfb75892e8f7fa
Marc Ladegourdie <40971471+marcldg@users.noreply.github.com>
2020-10-14 12:21:52 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require('mongoose');
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 80;
 const base = `${__dirname}/public`;
-
-//connecting to mongoDB
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://MARCLDG:SIT31153HD@cluster0.f3acr.mongodb.net/changestream?retryWrites=true&w=majority")
-
-//creating a dataschema
-var dataSchema = new mongoose.Schema({
-    _id: String,
-    mac_address: String,
-    status: Boolean,
-    level: Number
-  }, {collection: 'collection'});
-
-var Data = mongoose.model("collection", dataSchema);
 
 //Setting Cross-Origin Headers
 app.use((req, res, next) => {
@@ -29,31 +13,10 @@ app.use((req, res, next) => {
     next();
 });
 
-//Setting express to use body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cookieParser());
-
 app.use(express.static('public'));
 
-app.post('/update-lights', (req, res) => {
-    const{bulbip, bulbstatus} = req.body;
-    console.log(req.body)
-    Data.findOne({_id: bulbip}, function(err, document){
-        console.log(document)
-        document.status = bulbstatus;
-        document.save((err) => {
-            if (err) { console.log(err); }
-            });
-    })
-    return res.json({
-        success: true
-    });
-});
-
-app.get('/', async (req, res) => {
-    var bulbs = await Data.find({})
-    return res.send(bulbs)
+app.get('/', (req, res) => {
+    res.sendFile(`${base}/frontend.html`);
 });
 
 //Web Listener\\
